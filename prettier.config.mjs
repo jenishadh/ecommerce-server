6bdb7ee1dea0df4e43929c79336fe7cc14c3b2e5
jenishxadhikari <202624708+jenishadh@users.noreply.github.com>
2025-05-12 15:54:34 +0000

    chore: initial setup

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+const config = {
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  endOfLine: 'auto',
+};
+
+export default config;
